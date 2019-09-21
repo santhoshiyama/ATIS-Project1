ed68e0fe4d894d7a794d50260ac2224ec07772c5
Srujana Malisetti <43800993+SrujanaMalisetti@users.noreply.github.com>
2019-09-21 15:54:28 -0400

    Frontend display fix

--- assignment1/src/main.js
+++ assignment1/src/main.js
@@ -3,8 +3,7 @@ $("#myform").submit(
 			event.preventDefault();
 			var name = document.getElementById("exampleInputEmail1").value;
 			var name2 = document.getElementById("exampleInputEmail2").value;
-			var targetUrl = 'http://localhost:8080/load?fauth=' + name
-					+ '&sauth=' + name2;
+			var targetUrl = 'http://localhost:8080/load?fauth=' + name + '&sauth=' + name2;
 			
 			var form_data = $(this).serialize();
 			var cachedThis = this;
@@ -15,9 +14,17 @@ $("#myform").submit(
 				data : form_data,
 				dataType : "XML"
 			}).done(function(response) {
-				var url=$(response).find("binding uri").text();
-				$("#server-results").html(url);
-
+				var coi_level;
+				if((response >= 0) && (response <= 5)){
+					coi_level = "Low";
+				}else if((response > 5) && (response <= 15)){
+					coi_level = "Medium";
+				}else if((response > 15)){
+					coi_level ="High";
+				}
+				document.getElementById("server-results").innerHTML = 
+					"<b><u>Result</u></b><br>Number of co-authored papers by <b>"+name+"</b> and <b>"+name2+"</b> is: 
+							<b>"+response+"</b><br>Calculated COI Level is: <b>"+coi_level+"</b>";
 			});
 
 		});
